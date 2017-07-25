0a2b524a585a166e46b500fdd9e018e2254e4e3b
Andrew Kadesky <amkadesky@aol.com>
2017-07-25 14:27:14 -0500

    most recent version of the youtube challenge files

--- search.js
+++ search.js
@@ -1,26 +1,37 @@
-function showResponse(response){
-  var responesString = JSON.stringify(response, '', 2);
-  document.getElementById('response').innerHTML +=responseString;
+// Example 1: Use method-specific function
+var request = gapi.client.youtube.channels.list({'part': 'snippet', 'mine': 'true'});
+
+// Execute the API request.
+request.execute(function(response) {
+  console.log(response);
+});
+
+
+// Example 2: Use gapi.client.request(args) function
+var request = gapi.client.request({
+  'method': 'GET',
+  'path': "https://www.googleapis.com/youtube/v3/search",
+  'params': {'part': 'snippet', 'mine': 'true'}
+});
+// Execute the API request.
+request.execute(function(response) {
+  console.log(response);
+});
+
+function handleAPILoaded() {
+  $('#search-button').attr('disabled', false);
 }
 
-function onClientLoad() {
-  gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-function onYouTubeApiLoad() {
-  gapi.client.setApiKey('AIzaSyADDAnPK96vUAIv8F3adfmikp-5uZzKNA4');
-  search();
-}
-
-function search() {
+// Search for a specified string.
+function search(YT_SEARCH_URL) {
+  var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
-    part: 'snippet',
-    q: 'cats'
+    q: q,
+    part: 'snippet'
   });
-  request.execute(onSearchResponse);
-}
 
-function onSearchResponse(response){
-  showResponse(response);
+  request.execute(function(response) {
+    var str = JSON.stringify(response.result);
+    $('#search-container').html('<pre>' + str + '</pre>');
+  });
 }
-
