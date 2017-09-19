3c428beb108b054b6c550a2456faf4ccb835c632
Andrew Kadesky <amkadesky@aol.com>
2017-09-19 14:04:37 -0500

    initial commit

--- search.js
+++ search.js
@@ -1,29 +1,10 @@
-// Example 1: Use method-specific function
-var request = gapi.client.youtube.channels.list({'part': 'snippet', 'mine': 'true'});
-
-// Execute the API request.
-request.execute(function(response) {
-  console.log(response);
-});
-
-
-// Example 2: Use gapi.client.request(args) function
-var request = gapi.client.request({
-  'method': 'GET',
-  'path': "https://www.googleapis.com/youtube/v3/search",
-  'params': {'part': 'snippet', 'mine': 'true'}
-});
-// Execute the API request.
-request.execute(function(response) {
-  console.log(response);
-});
-
+// After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
 }
 
 // Search for a specified string.
-function search(YT_SEARCH_URL) {
+function search() {
   var q = $('#query').val();
   var request = gapi.client.youtube.search.list({
     q: q,
